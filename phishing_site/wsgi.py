fe4b9f44ee5efb8357396471c9907f678f78a62a
dkayaa <dkdilsiz@live.com>
2024-10-27 09:29:54 +1100

    adding wsgi and stuff to .gitignore

---
+++ phishing_site/wsgi.py
@@ -0,0 +1,11 @@
+
+import sys
+
+dep_path =  '/home/deniz_kaya/pvenv/myenv/lib/python3.11/site-packages'
+sys.path.insert(0, dep_path)
+
+
+# Add the application directory to the sys.path
+sys.path.insert(0, '/var/www/phishing_site')
+
+from app import app as application 
