67bfc38b76f1c2c7989e318ba7f3a4225837a39f
dkayaa <dkdilsiz@live.com>
2024-10-27 09:46:53 +1100

    Integration changes

--- configurator/activate_this.py
+++
@@ -1,6 +0,0 @@
-import sys
-import logging
-from app import app as application  # Import your Flask app
-
-# Add the application directory to the sys.path
-sys.path.insert(0, '/var/www/configurator')
--- configurator/app.py
+++ configurator/app.py
@@ -7,14 +7,14 @@ import subprocess
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'pineapple@2024'
 
-dnsmasq_path = './tests/dnsmasq2.conf'
-hostapd_path = './tests/hostapd2.conf'
-dnsmasq_leases_path = './tests/dnsmasq.leases'
+dnsmasq_path = '/etc/dnsmasq.conf'
+hostapd_path = '/etc/hostapd/hostapd.conf'
+dnsmasq_leases_path = '/var/www/configurator/tests/dnsmasq.leases'
 
 os.system(f"sudo chmod 644 {dnsmasq_path}")
 
 def get_db_connection():
-	conn = sqlite3.connect('resources/database.db')
+	conn = sqlite3.connect('/var/www/configurator/resources/database.db')
 	conn.row_factory = sqlite3.Row
 	return conn
 
@@ -51,90 +51,94 @@ def routing_page():
 			
 			#update dnsmasq config 
 			with open(dnsmasq_path, 'w') as file:
-				file.write("""
-interface=wlan0
+				file.write("""interface=wlan0
 #define 18 address leases for a duration of 24 hours 
 dhcp-range=192.168.29.2,192.168.29.30,255.255.255.0,24h
-				"""	
+#domain=wlan
+#prevent dnsmasq from using resolv.conf(which configures the systems DNS resolver)
+#no-resolv
+#DNS Mappings
+#configurator
+address=/mypineapple.config.local/192.168.5.1"""	
 				)
 				conn = get_db_connection()
 				domains2 = conn.execute('SELECT * FROM domains').fetchall()
 				for domain in domains2:
 					if domain['domain_url'] and domain['domain_url']:
 						file.write(f"""
-
 #Website {domain['domain_url']}
 address=/{domain['domain_url']}/{domain['domain_ip']}
-						"""
+#no upstream dns server for this url
+server=/{domain['domain_url']}/0.0.0.0"""
 					)
 				conn.close()	
 			
 			#restart dnsmasq
-			#os.system(f"sudo systemctl restart dnsmasq")
+			os.system(f"sudo systemctl restart dnsmasq")
 			return redirect(url_for('routing_page'))
 	return render_template('routing_page.html', domains=domains, domain_ips=domain_ips)
 		
 @app.route('/clients_page')
 def clients_page():
-	#execute the below , parse and store in client table.
-	#iw dev wlan0 station dump | grep ^.*Station.*$
-	try:
-		# Execute the command
-		result = subprocess.run(['iw', 'dev', 'wlan0', 'station', 'dump'], stdout=subprocess.PIPE, text=True)
-		
-		lines = result.stdout.split('\n')
-		mac_addresses = [line.split()[1] for line in lines if line.startswith('Station')]
-		conn = get_db_connection()
-		conn.execute("UPDATE clients SET is_connected = \"FALSE\"")
-		for mac in mac_addresses:
-			conn.execute("INSERT OR IGNORE INTO clients (client_id, is_connected) VALUES (?, ?)", (mac, 'TRUE'))
-			conn.execute("UPDATE clients SET is_connected = \"TRUE\", modified = CURRENT_TIMESTAMP WHERE client_id = ?", (mac))
-		conn.commit()
-		conn.close()
-	except Exception as e:
-		print(f"Exception Raised: {e}")
-
-	return render_template('clients_page.html')
+    #execute the below , parse and store in client table.
+    #iw dev wlan0 station dump | grep ^.*Station.*$
+    conn = get_db_connection()
+    try:
+        # Execute the command
+        result = subprocess.run(['iw', 'dev', 'wlan0', 'station', 'dump'], stdout=subprocess.PIPE, text=True)
+        lines = result.stdout.split('\n')
+        mac_addresses = [line.split()[1] for line in lines if line.startswith('Station')]
+        #conn.execute("UPDATE clients SET is_connected = \"FALSE\"")
+        conn.execute('DELETE FROM clients')
+        print(mac_addresses)
+        for mac in mac_addresses:
+            conn.execute("INSERT OR IGNORE INTO clients (client_id, is_connected) VALUES (?, ?)", (mac, 'TRUE'))
+            #conn.execute("UPDATE clients SET is_connected = \"TRUE\", modified = CURRENT_TIMESTAMP WHERE client_id = ?", (mac))
+        conn.commit()
+    except Exception as e:
+        print(f"Exception Raised: {e}")
+    clients = conn.execute('SELECT * FROM clients').fetchall()
+    conn.close()
+    return render_template('clients_page.html', clients=clients)
 
 @app.route('/traffic_page', methods=('GET', 'POST'))
 def traffic_page():
-	return render_template('traffic_page.html')
+    return render_template('traffic_page.html')
 
 @app.route('/configs_page', methods=('GET', 'POST'))
 def configs_page():
 
-	conn = get_db_connection()
-	configs = conn.execute('SELECT * FROM configs').fetchall()
-	config_ids = conn.execute('SELECT config_id FROM configs').fetchall()
+    conn = get_db_connection()
+    configs = conn.execute('SELECT * FROM configs').fetchall()
+    config_ids = conn.execute('SELECT config_id FROM configs').fetchall()
 
-	conn.close()
+    conn.close()
 
-	if request.method == 'POST':
+    if request.method == 'POST':
 
-		config_value = request.form['config_value'].strip()
-		config_id = request.form['config_id'].strip()
+        config_value = request.form['config_value'].strip()
+        config_id = request.form['config_id'].strip()
 		
-		if not config_id:
-			flash('Config ID is required!')
-		else:
-			conn = get_db_connection()
-			conn.execute(
+        if not config_id:
+            flash('Config ID is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute(
 				""" UPDATE configs  
 				SET config_value = ?, 
 				modified = CURRENT_TIMESTAMP 
 				WHERE  
 				config_id = ? """, (config_value, config_id))
-			conn.commit()
-			conn.close()
-			
-			#update hostapd
-			conn = get_db_connection()
-			with open(hostapd_path, 'w') as file:
-				config_device_ssid = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.device_ssid\"").fetchall()[0]
-				config_device_hasPassword = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.device_hasPassword\"").fetchall()[0]
-				config_device_password = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.device_password\"").fetchall()[0]					
-				if config_device_ssid['config_value']:
-					file.write(f"""
+            conn.commit()
+            conn.close()
+            #update hostapd
+            conn = get_db_connection()
+            with open(hostapd_path, 'w') as file:
+                config_device_ssid = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.device_ssid\"").fetchall()[0]
+                config_device_hasPassword = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.device_hasPassword\"").fetchall()[0]
+                config_device_password = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.device_password\"").fetchall()[0]
+                if config_device_ssid['config_value']:
+                    file.write(f"""
 # the interface used by the AP
 interface=wlan0
 driver=nl80211
@@ -154,34 +158,30 @@ wmm_enabled=1
 ssid={config_device_ssid['config_value'].strip()}
 # 1=wpa, 2=wep, 3=both
 auth_algs=1
-ignore_broadcast_ssid=0
-					""")
+ignore_broadcast_ssid=0""")
 					
-				if config_device_hasPassword['config_value'] == 'TRUE': 
-					file.write(f"""
+                if config_device_hasPassword['config_value'] == 'TRUE': 
+                    file.write(f"""
 # WPA2 only
 wpa=2
 wpa_passphrase={config_device_password['config_value'].strip()}
 wpa_key_mgmt=WPA-PSK
 wpa_pairwise=TKIP
-rsn_pairwise=CCMP
-					""")
+rsn_pairwise=CCMP""")
 			#restart hostapd 
-			#os.system(f"sudo systemctl restart hostapd")
-
-			
-			config_parent_ssid = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.parent_ssid\"").fetchall()[0]
-			config_parent_hasPassword = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.parent_hasPassword\"").fetchall()[0]
-			config_parent_password = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.parent_password\"").fetchall()[0]			
-			if (config_parent_ssid['config_value']):
-				if (config_parent_hasPassword['config_value']) == 'TRUE' and config_parent_password['config_value']:
-					#protected SSID
-					#os.system(f"nmcli device wifi connect \"{config_parent_ssid['config_value']}\" password \"{config_parent_password['config_value']}\"")
-					print(f"nmcli device wifi connect \"{config_parent_ssid['config_value'].strip()}\" password \"{config_parent_password['config_value'].strip()}\"")
-				else:
-					#open SSID
-					#os.system(f"nmcli device wifi connect \"{config_parent_ssid['config_value']}\"")
-					print(f"nmcli device wifi connect \"{config_parent_ssid['config_value'].strip()}\"")
-			conn.close()	
-			return redirect(url_for('configs_page'))
-	return render_template('configs_page.html', configs=configs, config_ids=config_ids)
+            os.system(f"sudo systemctl restart hostapd")
+            config_parent_ssid = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.parent_ssid\"").fetchall()[0]
+            config_parent_hasPassword = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.parent_hasPassword\"").fetchall()[0]
+            config_parent_password = conn.execute("SELECT * FROM configs WHERE config_id = \"configs.parent_password\"").fetchall()[0]			
+            if (config_parent_ssid['config_value']):
+                if (config_parent_hasPassword['config_value']) == 'TRUE' and config_parent_password['config_value']:
+                    #protected SSID
+                    os.system(f"nmcli device wifi connect \"{config_parent_ssid['config_value']}\" password \"{config_parent_password['config_value']}\"")
+                    print(f"nmcli device wifi connect \"{config_parent_ssid['config_value'].strip()}\" password \"{config_parent_password['config_value'].strip()}\"")
+                else:
+                    #open SSID
+                    os.system(f"nmcli device wifi connect \"{config_parent_ssid['config_value']}\"")
+                    print(f"nmcli device wifi connect \"{config_parent_ssid['config_value'].strip()}\"")
+            conn.close()	
+            return redirect(url_for('configs_page'))
+    return render_template('configs_page.html', configs=configs, config_ids=config_ids)
