a60b4bce2cc957eb8f5501ce727ec3dbd8e92efb
Deniz Dilsiz <dkdilsiz@live.com>
2024-11-01 17:19:56 +1100

    more cleanup

--- configurator/app.py
+++ configurator/app.py
@@ -2,13 +2,6 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 import os
 import subprocess
-
-#from scapy.all import sniff
-#from scapy.layers.http import HTTPRequest, HTTPResponse
-#from scapy.all import conf
-
-#import threading
-#import time
 import csv
 import subprocess 
 
@@ -24,28 +17,6 @@ db_connection_string = '/var/www/configurator/resources/database.db'
 
 #traffic_csv = './resources/traffic'
 traffic_csv = '/var/www/configurator/resources/traffic'
-
-# Define a callback function to process captured packets
-#def process_packet(packet):
-#    with open(traffic_csv, 'a') as f:
-#        if packet.haslayer(HTTPRequest):
-#            http_request = packet[HTTPRequest]
-#            f.write(f"HTTP Request\t{http_request.Method.decode()}\t {http_request.Host.decode()}\t {http_request.Path.decode()} \n")
-#            #print(f"HTTP Request\t {http_request.Method.decode()}\t {http_request.Host.decode()}\t {http_request.Path.decode()}")
-#        elif packet.haslayer(HTTPResponse):
-#            http_response = packet[HTTPResponse]
-#            f.write(f"HTTP Response\t {http_response.Status_Code.decode()}\t \t \n")
-#            #print(f"HTTP Response\t {http_response.Status_Code.decode()}\t \t")
-
-#def traffic_process():
-    # Start sniffing on the desired interface (replace 'eth0' with your interface)
-    #sniff(iface='en0', filter='tcp port 80', prn=process_packet, store=0)
-   # sniff(iface='wlan1', filter='tcp port 80', prn=process_packet, store=0)
-
-
-
-#thread = threading.Thread(target=traffic_process)
-#thread.start()
 
 command = ['sudo', '/home/deniz_kaya/pvenv/myenv/bin/python3.11', '/var/www/configurator/sniffer.py']
 subprocess.Popen(command) 
