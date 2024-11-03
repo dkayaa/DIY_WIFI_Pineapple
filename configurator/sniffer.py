9389d27b1b6b42e241b260d170e58b8cd8f16122
Deniz Dilsiz <dkdilsiz@live.com>
2024-11-03 14:08:24 +1100

    last changes?

--- configurator/sniffer.py
+++ configurator/sniffer.py
@@ -4,21 +4,19 @@ from scapy.layers.http import HTTPRequest, HTTPResponse
 from scapy.all import conf
 
 import threading
+from datetime import datetime
 
 traffic_csv = '/var/www/configurator/resources/traffic'
 #traffic_csv = './resources/traffic'
 
 # Define a callback function to process captured packets
 def process_packet(packet):
-    with open(traffic_csv, 'a') as f:
-        if packet.haslayer(HTTPRequest):
-            http_request = packet[HTTPRequest]
-            f.write(f"HTTP Request\t{http_request.Method.decode()}\t {http_request.Host.decode()}\t {http_request.Path.decode()} \n")
-            #print(f"HTTP Request\t {http_request.Method.decode()}\t {http_request.Host.decode()}\t {http_request.Path.decode()}")
-        elif packet.haslayer(HTTPResponse):
-            http_response = packet[HTTPResponse]
-            f.write(f"HTTP Response\t {http_response.Status_Code.decode()}\t \t \n")
-            #print(f"HTTP Response\t {http_response.Status_Code.decode()}\t \t")
+    with open(traffic_csv, 'a') as f:       
+        if packet is not None:
+            f.write(datetime.now().strftime("%d/%m/%Y %H:%M:%S") + f"\t" + \
+                    packet.sniffed_on + f"\t" + \
+                    packet.summary() + f"\t\n")
+
 
 def traffic_process():
     # Start sniffing on the desired interface (replace 'eth0' with your interface)
